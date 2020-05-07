30a49c890ea0539ce22ad9a244ec488d05fd40c0
Nick-Vecchio <nick_vecchio@yahoo.com>
2020-05-06 20:07:09 -0400

    latest update of site

--- js/search.js
+++ js/search.js
@@ -1,9 +1,16 @@
 // JavaScript Document
-$(document).ready(function(){
-        $("#myinputdata").on("keyup",function(){
-          var value=$(this).val().toLowerCase();
-           $(".mydivdata *").filter(function(){
-            $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1)
-          });
-        });
-     });
+function myFunction() {
+    var input, filter, cards, cardContainer, h4, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body h4.card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
